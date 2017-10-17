481403e153494d1317cefb36adee4497ed7a78a4
arikolsh <arikolsh@gmail.com>
2017-10-17 06:32:03 +0300

    add project

---
+++ problem-solving/src/main/java/com/arik/problemsolving/ProblemSolvingApplication.java
@@ -0,0 +1,12 @@
+package com.arik.problemsolving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemSolvingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemSolvingApplication.class, args);
+	}
+}
